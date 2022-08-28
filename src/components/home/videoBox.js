c1123028183b2937954808dbd683f12fe6222a1d
VASU <81980522+VasuTech01@users.noreply.github.com>
2022-08-28 13:52:36 +0530

    Added Turn Servers
    changed stun and turn serve urls

--- src/components/home/videoBox.js
+++ src/components/home/videoBox.js
@@ -45,7 +45,26 @@ class VideoBox extends Component {
   }
   initConnection(stream) {
     const server = {
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+      iceServers: [
+    {
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+  ],
     };
     const localConnection = new RTCPeerConnection(server);
     this.state.stream.getTracks().forEach((track) => {
